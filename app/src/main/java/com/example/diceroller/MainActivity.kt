83382e47547bb568538c2329d41b801364f04e20
victor.barbulescu23 <victor.barbulesu23@d155.org>
2022-10-19 07:51:58 -0500

    rolls a "die" and generates a random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -25,7 +26,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randNum = Random.nextInt(6) + 1
-        val diceRoll: TextView = findViewById(R.id.roll_number)
-        diceRoll.text = randNum.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+
+        diceImage.setImageResource(drawableResource)
+
     }
 }
