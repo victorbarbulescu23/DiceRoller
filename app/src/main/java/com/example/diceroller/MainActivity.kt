21ad79c61fbf85951d514fe963752e055fdd3636
victor.barbulescu23 <victor.barbulesu23@d155.org>
2022-10-19 08:04:35 -0500

    rolls a "die" and generates a random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,26 +9,27 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-
+        rollButton = findViewById(R.id.roll_button)
         rollButton.text = "Push Me"
-
         rollButton.setOnClickListener{
             Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
 
+        diceImage = findViewById(R.id.dice_image)
+
     }
 
     private fun rollDice() {
-        val randNum = Random.nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-
-        val drawableResource = when (randNum){
+        val drawableResource = when (Random.nextInt(6) + 1){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
